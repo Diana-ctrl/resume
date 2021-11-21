d2d322068492cb1c4865b3e0f432bcd7f1078421
Diana <diana.lyah.96@mail.ru>
2021-11-21 23:50:46 +0300

    Add componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -14,12 +14,11 @@ type UsersPropsType = {
 }
 
 class Users extends React.Component<UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props)
-        if (this.props.usersPage.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items));
-        }
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items));
     }
+
     render() {
         return <div className={classes.item}>
             {this.props.usersPage.map(user =>
